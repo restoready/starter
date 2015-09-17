f469d721cb24660f4931596f4b2f602afe16f8a0
Kévin Bizien <bizienkevin@gmail.com>
2015-09-17 16:34:21 +0200

    scss architecture & gulp-concatenation

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -7,19 +7,58 @@ var concat = require('gulp-concat');
 
 gulp.task('dev:sass', function () {
   return gulp.src([
-    './scss/settings/',
-    './scss/tools/',
-    './scss/generic/',
-    './scss/base/',
-    './scss/objects/',
-    './scss/components/',
-    './scss/hacks/'
+    // Settings
+    './scss/settings/_author.scss',
+    './scss/settings/_general.scss',
+    './scss/settings/_colors.scss',
+    './scss/settings/_breakpoints.scss',
+
+    // Tools
+    './scss/tools/_mixins.scss',
+
+    // Generic
+    './scss/generic/_reset.scss',
+    './scss/generic/_box-model.scss',
+
+    // Base
+    './scss/base/_basic-styles.scss',
+    './scss/base/_typography.scss',
+    './scss/base/_lists.scss',
+    './scss/base/_forms.scss',
+    './scss/base/_fontcustom.scss',
+
+    // Object
+    './scss/objects/_images-iframes.scss',
+    './scss/objects/_buttons.scss',
+
+    // Components
+    './scss/components/_form.scss',
+    './scss/components/_layout.scss',
+    './scss/components/_header.scss',
+    './scss/components/_nav.scss',
+    './scss/components/_footer.scss',
+    './scss/components/_custom-page.scss',
+    './scss/components/_menu.scss',
+    './scss/components/_food.scss',
+    './scss/components/_back-to-menu.scss',
+    './scss/components/_tags.scss',
+    './scss/components/_news.scss',
+    './scss/components/_gallery.scss',
+    './scss/components/_venobox.scss',
+
+    // Pages
+    './scss/pages/_home.scss',
+    './scss/pages/_contact.scss'
+
+    // Hacks
+
   ])
   .pipe(concat('application.scss.liquid'))
   .pipe(gulp.dest('../assets/'));
 });
 
 gulp.task('watch', function () {
+  // SCSS
   watch('./scss/**/*.scss', batch(function (events, done) {
     gulp.start('dev:sass', done);
   }));
